a0d9baee40dc1bbf9de7a69783bd1a5a5baa3c7f
Sanghyeok Park <87484408+huhaha9142@users.noreply.github.com>
2021-10-21 16:52:07 +0900

    게시물 작성 구현

--- src/main/java/com/board/controller/BoardController.java
+++ src/main/java/com/board/controller/BoardController.java
@@ -19,13 +19,26 @@ public class BoardController {
 	@Inject
 	private BoardService service;
 
+	//	게시물 목록
 	@RequestMapping(value="/list", method=RequestMethod.GET)
 	public void getList(Model model) throws Exception {
 		
-		List<BoardVO> list = null;
-		list = service.list();
-		
+		List<BoardVO> list = null; list = service.list();
 		model.addAttribute("list", list);
 		
+	}
+	
+	//	게시물 작성
+	@RequestMapping(value = "/write", method = RequestMethod.GET)
+	public void getWrite() throws Exception {
+	
+	}
+	
+	// 게시물 작성
+	@RequestMapping(value = "/write", method = RequestMethod.POST)
+	public String postWrite(BoardVO vo) throws Exception {
+	  service.write(vo);
+	  
+	  return "redirect:/board/list";
 	}
 }
